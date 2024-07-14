6c639d24db575df27b9395da1e3f224d98e68434
Ilya Stritskiy <msv_ilya@mail.ru>
2024-07-14 13:06:14 +0300

    task 5 was solved

---
+++ task_5.js
@@ -0,0 +1,10 @@
+// стрелочная функция
+let getNumberToPower = (x, n) => {
+    let result = x ** n;
+    return result;
+};
+
+
+// вызов функции
+console.log(getNumberToPower(25, 0))
+console.log(getNumberToPower(2, 5))
